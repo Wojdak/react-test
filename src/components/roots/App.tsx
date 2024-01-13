1ef7ea62c1f94df9ad2e0e44f4b5d7d80cc307ce
Wojdak <jakub.wojdak11@gmail.com>
2024-01-13 13:10:56 +0100

    Initial commit

---
+++ src/components/roots/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import NavBar from '../common/NavBar';
+import PostsPage from './PostsPage';
+import CommentsPage from './CommentsPage';
+import AlbumsPage from './AlbumsPage';
+import PhotosPage from './PhotosPage';
+import UsersPage from './UsersPage';
+import TodosPage from './TodosPage';
+
+function App() {
+  return (
+    <Router>
+    <NavBar />
+    <Routes>
+      <Route path="/posts" element={<PostsPage />} />
+      <Route path="/comments/:postId" element={<CommentsPage />} />
+      <Route path="/albums" element={<AlbumsPage />} />
+      <Route path="/photos/:albumId" element={<PhotosPage />} />
+      <Route path="/users" element={<UsersPage />} />
+      <Route path="/todos/:userId" element={<TodosPage />} />
+      {/* Define other routes similarly */}
+    </Routes>
+  </Router>
+  );
+}
+
+export default App;
